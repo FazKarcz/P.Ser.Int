087e6d0ef16a3fb51828503706dbdf3d7ff47859
Jakub Karcz <92019651+FazKarcz@users.noreply.github.com>
2023-10-18 18:36:48 +0200

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -34,27 +34,3 @@ if check_url("https://www.meteoprog.pl/pl/weather/Olsztyn/"):
     plt.ylabel('Temperatura (°C)')
     plt.title('temperatury')
     plt.show()
-
-#ZAD3
-url = f'https://api.gios.gov.pl/pjp-api/rest/data/getData/644'
-response1 = requests.get(url)
-lista3 = []
-lista4 = []
-if response1.status_code != 200:
-  assert False
-
-data = json.loads(response1.content.decode('utf-8'))
-for x in range(min(len(data["values"]), 10)):
-    lista3.append(data['values'][x]['value'])
-    lista4.append(data['values'][x]['date'])
-
-
-
-nazwy = []
-for item in lista4:
-    nazwy.append(item[11:])
-plt.plot(lista3,nazwy)
-plt.xlabel('wartosc')
-plt.ylabel('godzina')
-plt.title('wartosc czujnika dla ostatnich 10 godzin')
-plt.show()
